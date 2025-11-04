546f10e440c1d6ead43ec9281d47701ce84e58d4
Oleksandr <aalexandr397@gmail.com>
2025-11-04 20:21:42 +0000

    more in future, I forgot HTML

---
+++ EL Nightreigh Builder/Pages/Builds.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EL_Nightreigh_Builder.Pages
+{
+    public class BuildsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ EL Nightreigh Builder/Pages/Runes.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EL_Nightreigh_Builder.Pages
+{
+    public class RunesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
